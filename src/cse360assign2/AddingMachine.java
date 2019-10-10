d42cad321ac9bfb517c599672b8f838baad3b3f0
iesalas1 <iesalas1@asu.edu>
2019-10-09 17:30:26 -0700

    First Version

---
+++ src/cse360assign2/AddingMachine.java
@@ -0,0 +1,46 @@
+/*
+Isaac Salas
+CSE360 Mon 9:40am-10:30am
+Assignment 1
+This package contains a AddingMachine.
+*/
+
+package cse360assign2;
+/*
+This class is an adding machine which has an int total, a constructor
+getTotal, and 4 methods that act as operators of the adding machine. 
+*/
+public class AddingMachine {
+        //**Data**
+    
+	private int total;
+	
+        //**Constructor**
+        
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	//**Methods**
+        
+        //Returns total 
+	public int getTotal () {
+		return 0;
+	}
+	
+        //this is the '+' operator
+	public void add (int value) {
+		
+	}
+	//this is the '-' operator
+	public void subtract (int value) {
+		
+	}
+	//returns a string of the equation	
+	public String toString () {
+		return "";
+	}
+        //clears the current equation
+	public void clear() {
+	
+	}
+}
---
+++ src/cse360assign2/Cse360assign2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cse360assign2;
+
+/**
+ *
+ * @author isaactheking7
+ */
+public class Cse360assign2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
